55e353975ec03143e26d66de5537e0925caf1897
RaulORios <raul.orios89@outlook.com>
2021-09-29 14:18:27 -0300

    Arquivos atualizados

---
+++ src/Orient_Objeto_Modif_Parametro_Out/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Orient_Objeto_Modif_Parametro_Out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
